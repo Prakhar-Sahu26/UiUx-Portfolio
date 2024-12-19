d7a92faccbfc059e863c4fe54b10008421b1fd94
Prakhar-Sahu26 <prakhar.26.sahu@gmail.com>
2024-12-19 20:03:25 +0530

    Update projects.js

--- html/projects.js
+++ html/projects.js
@@ -1,21 +1,43 @@
 // Get the popup elements
 const popupContainer = document.getElementById('popupContainer');
+const popupTitle = document.getElementById('popupTitle');
+const videoSource = document.getElementById('videoSource');
+const popupVideo = document.getElementById('popupVideo');
 const openPopup = document.getElementById('openPopup');
+const openPopup1 = document.getElementById('openPopup1');
+const openPopup2 = document.getElementById('openPopup2');
 const closePopup = document.getElementById('closePopup');
 
-// Open the popup
+// Open the popup with different content
 openPopup.addEventListener('click', () => {
-    popupContainer.style.display = 'flex';
+  popupTitle.textContent = 'Shoping Website';
+  Image.src = '../video2.jpg' // Add your video path here
+  popupVideo.load(); // Reload the video with the new source
+  popupContainer.style.display = 'flex';
+});
+
+openPopup1.addEventListener('click', () => {
+  popupTitle.textContent = 'Fashion Website';
+  videoSource.src = '../video3.mp4'; // Add your video path here
+  popupVideo.load(); // Reload the video with the new source
+  popupContainer.style.display = 'flex';
+});
+
+openPopup2.addEventListener('click', () => {
+  popupTitle.textContent = 'Fashion Landing Page';
+  videoSource.src = '../video1.mp4' // Add your video path here
+  popupVideo.load(); // Reload the video with the new source
+  popupContainer.style.display = 'flex';
 });
 
 // Close the popup
 closePopup.addEventListener('click', () => {
-    popupContainer.style.display = 'none';
+  popupContainer.style.display = 'none';
 });
 
 // Close the popup by clicking outside the modal
 popupContainer.addEventListener('click', (e) => {
-    if (e.target === popupContainer) {
-        popupContainer.style.display = 'none';
-    }
+  if (e.target === popupContainer) {
+    popupContainer.style.display = 'none';
+  }
 });
